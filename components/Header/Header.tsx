3cd729adf4a5729b450a819707263358caad7243
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-09-29 22:40:00 +0700

    style: :lipstick: add margin for header and div to contain the logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -11,7 +11,7 @@ import {
 type Props = {};
 const Header = (props: Props) => {
   return (
-    <header className="flex h-auto flex-col items-center justify-between sm:flex-row">
+    <header className="m-2 flex h-auto flex-col items-center justify-between sm:flex-row">
       <div className="flex max-w-2xl flex-grow justify-evenly">
         <HeaderItem title="HOME" Icon={HomeIcon}></HeaderItem>
         <HeaderItem title="TRENDING" Icon={BoltIcon}></HeaderItem>
@@ -20,14 +20,15 @@ const Header = (props: Props) => {
         <HeaderItem title="COLLECTION" Icon={RectangleStackIcon}></HeaderItem>
         <HeaderItem title="USER" Icon={UserIcon}></HeaderItem>
       </div>
-
-      <Image
-        src="https://links.papareact.com/ua6"
-        width={200}
-        height={200}
-        alt="Logo"
-        className="object-contain"
-      ></Image>
+      <div>
+        <Image
+          src="https://links.papareact.com/ua6"
+          width={200}
+          height={200}
+          alt="Logo"
+          className="object-contain"
+        ></Image>
+      </div>
     </header>
   );
 };
