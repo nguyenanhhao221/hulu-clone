46446304c75e52015543b4b5d1203b3fafa309c6
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-10-08 19:46:59 +0700

    feat: Add Writer components for TV.
    -Only for TV, the Movie need to make another API call to display correctly

---
+++ src/components/MoviesDetail/Writers.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TMovie } from '../../../type';
+
+type Props = {
+  created_by?: TMovie['created_by'];
+};
+
+const Writers = ({ created_by }: Props) => (
+  <ul className="flex w-4/5 flex-wrap items-center justify-between gap-2">
+    {created_by?.map((writer) => (
+      <li className="text-left" key={writer.id}>
+        <p className="font-bold">{writer.name}</p>
+        <p>Creator</p>
+      </li>
+    ))}
+  </ul>
+);
+
+export default Writers;
